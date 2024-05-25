a7a18626c8b192a3b81bba142d8ba5b28f010bf7
me-engi <tapansarker903@gmail.com>
2024-05-26 01:02:38 +0530

    user_added

--- ghumneyho/settings.py
+++ ghumneyho/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'ghumneyho.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'salad',
+        'USER': 'sarkar',
+        'PASSWORD': '*Nepal8910#',
+        'HOST': 'localhost',   # Or the IP address where your MySQL server is hosted
+        'PORT': '3306',        # Default MySQL port
     }
 }
 
